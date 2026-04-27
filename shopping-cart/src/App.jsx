b3961e1e4053b4f04d14c213a90ce8e2c0139844
Vedat <vedat.arpa.72@gmail.com>
2026-04-27 22:36:40 +0300

    seting pages and tailwind

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import Cart from './pages/cart'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-
+      <h1>Navbar</h1>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
     </div>
   )
 }
